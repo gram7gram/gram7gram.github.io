cf007bd0d35dd4f0b314290b837fda51c2c027a3
Gram <gram7gram@gmail.com>
2021-02-07 18:18:32 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,163 @@
+const download = (output, { name, string }) => {
+  const li = document.createElement('li');
+  const href = document.createElement('a');
+  href.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(string));
+  href.setAttribute('download', name);
+  href.innerText = name;
+
+  li.appendChild(href);
+  output.appendChild(li);
+}
+
+const cleanRow = row => {
+  const content = {}
+
+  Object.entries(row).forEach(([key, value]) => {
+    content[key] = `${value}`.trim()
+  })
+
+  return content;
+}
+
+const parseItems = (sheet) => {
+
+  const end = sheet['!ref'].split(':')[1]
+
+  const json = XLSX.utils.sheet_to_json(sheet, {
+    range: `A2:${end}`
+  })
+
+  const data = json.map(cleanRow)
+
+  return {
+    total: data.length,
+    data,
+  }
+}
+
+const parseStrings = (sheet) => {
+
+  const json = XLSX.utils.sheet_to_json(sheet, {
+    range: sheet['!ref']
+  })
+
+  const data = json.map(cleanRow)
+
+  return {
+    total: data.length,
+    data,
+  }
+}
+
+const parseExpGold = (sheet) => {
+
+  const end = sheet['!ref'].split(':')[1]
+
+  const json = XLSX.utils.sheet_to_json(sheet, {
+    range: `A2:${end}`
+  })
+
+  const data = json.map(cleanRow)
+
+  return {
+    total: data.length,
+    data,
+  }
+}
+
+const parseAchievements = (sheet) => {
+
+  const json = XLSX.utils.sheet_to_json(sheet, {
+    range: sheet['!ref']
+  })
+
+  const data = json.map(cleanRow)
+
+  return {
+    total: data.length,
+    data,
+  }
+}
+
+const parseRanks = (sheet) => {
+
+  const json = XLSX.utils.sheet_to_json(sheet, {
+    range: sheet['!ref']
+  })
+
+  const data = json.map(cleanRow)
+
+  return {
+    total: data.length,
+    data,
+  }
+}
+
+const parse = (buffer) => {
+
+  let workbook = XLSX.read(buffer, { type: 'buffer' })
+
+  const handlers = {
+    'Item Stats': parseItems,
+    'Strings': parseStrings,
+    'Experience and Gold': parseExpGold,
+    'Achievements': parseAchievements,
+    'Ranks': parseRanks,
+  }
+
+  const values = {}
+
+  Object.entries(handlers).forEach(([name, handler]) => {
+    const sheet = workbook.Sheets[name];
+    if (sheet !== undefined) {
+      values[name] = handler(sheet);
+    }
+
+  })
+
+  return {
+    values,
+    sheets: Object.keys(handlers)
+  }
+}
+
+const init = () => {
+  const upload = document.getElementById('upload')
+  const output = document.getElementById('output')
+
+  upload.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (!file) return false;
+
+    output.innerHTML = ''
+
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(file);
+
+    reader.onload = () => {
+      const result = parse(reader.result);
+
+      result.sheets.forEach(sheet => {
+        const json = result.values[sheet];
+        const string = JSON.stringify(json, null, 2)
+
+        download(output, {
+          name: `${sheet}.json`,
+          string
+        })
+      })
+    }
+
+    reader.onprogress = (e) => {
+      const progress = e.total > 0 ?  e.loaded / e.total : 0;
+      console.log({ progress });
+    }
+
+    reader.onerror = () => {
+      console.log(reader.error);
+
+    }
+  })
+}
+
+window.addEventListener('load', init);
